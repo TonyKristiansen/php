1463d15f4d2f2bc11422a1cfd69a379b80155668
tmknetdev <63122515+tmknetdev@users.noreply.github.com>
2020-04-24 18:12:31 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+  </head>
+
+  <body class="text-center">
+    <table><tr><td><form class="form-signin">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Realize o login</h1>
+      <label for="inputEmail" class="sr-only">Número do celular</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Número do celular" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Me lembre
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Registrar</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form></td></tr></table>
+    
+  </body>
+</html>
